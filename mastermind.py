780f53fcb022f6b61b651ce48c4c11bc3bb99b6e
elhadji <elmoussaseck298@gmail.com>
2026-09-13 20:26:39 +0200

    début de notre programme

---
+++ mastermind.py
@@ -0,0 +1,12 @@
+import random
+
+COLORS = {
+    "R": "Rouge",
+    "V": "Vert",
+    "B": "Bleu",
+    "J": "Jaune",
+}
+
+def afficher_couleurs():
+    for lettre, nom in COLORS.items():
+        print(f"{lettre} = {nom}")
