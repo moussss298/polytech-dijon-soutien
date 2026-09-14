cf8776a2e046c88d65a8f0c07531fe788b3e0f28
elhadji <elmoussaseck298@gmail.com>
2026-09-14 15:47:08 +0200

    partie 1-2

--- mastermind.py
+++ mastermind.py
@@ -1,19 +1,21 @@
 import random
 
-COLORS = {
+COULEURS = {
     "R": "Rouge",
     "V": "Vert",
     "B": "Bleu",
     "J": "Jaune",
+    "M": "Mauve",
+    "N": "Noir",
 }
 
-def afficher_couleurs():
-    for lettre, nom in COLORS.items():
-        print(f"{lettre} = {nom}")
-<<<<<<< HEAD
-        
-=======
-qfgfqg
-fsgqgnjg
-gsghhhhd
->>>>>>> b91c1566a00e3b25a9ce3ba4ba1284e8264e1241
+# Afficher les couleurs disponibles
+for lettre, nom in COULEURS.items():
+    print(f"{lettre} = {nom}")
+
+# Créer le code secret
+code_secret = []
+
+for i in range(4):
+    couleur = random.choice(list(COULEURS.keys()))
+    code_secret.append(couleur)
